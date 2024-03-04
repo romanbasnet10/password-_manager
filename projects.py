671dcd76f0d8523c19b6b764032c603ef939caca
roman basnet <romanbasynet123@gmail.com>
2024-03-05 00:49:40 +0545

    saving and gettin password

--- projects.py
+++ projects.py
@@ -11,5 +11,31 @@ class password_manager:
         with open(path,"wb") as f:
             f.write(self.key)
 
-pm=password_manager()
-pm.create_key("mykey.key")
+    def load_key(self,path):
+        with open (path,"rb") as f:
+            self.key=f.read()
+
+    def create_password_file(self,path,intial_values=None):
+        self.password_file=path
+
+        if intial_values is not None:
+            for key,value in initial_values.items():
+                self.add_password(key,value)
+    def load_password_file(self,path):
+        self.password_file=path
+
+        with open(path,"r")as f:
+            for line in f:
+                site,encrypted=line.split(":")
+                self.password_dict[site]=fernet(self.key).decrypted(encrypted.encoded())
+    
+    def add_password (self,site,password):
+        self.password_dict[site]=password
+
+        if self.password_file is not None:
+            with open (self.password_file,"a+")as file:
+                 encrypted= fernet(self,key).encerypt(password_encode())
+                 f.write(site+":"+encrypted.decode()+"\n")
+    
+    def get_password(self,site):
+            return self.password_dict[site]
